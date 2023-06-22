f538144c3e2cf925378b594db0af99d08666218d
vivian <vivian.govender@live.com>
2023-06-22 07:54:18 -0700

    t0

---
+++ README.md
@@ -0,0 +1,1 @@
+Directory for the RSA-Factoring-Challenge
---
+++ head.h
@@ -0,0 +1,12 @@
+#ifndef _HEAD_H_
+#define _HEAD_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define _GNU_SOURCE
+
+int factorize(char *buffer);
+
+#endif
